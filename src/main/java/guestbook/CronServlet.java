737c6a935f040ddb5dd39cda73489e2986f94489
Tarang Khandpur <TKnologic104@gmail.com>
2017-02-15 23:03:18 -0600

    cron llamas

--- src/main/java/guestbook/CronServlet.java
+++ src/main/java/guestbook/CronServlet.java
@@ -1,5 +1,5 @@
 package guestbook;
-
+//
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Collections;
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
-
+//
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -19,9 +19,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+//
 import com.googlecode.objectify.ObjectifyService;
-
+import com.sendgrid.SendGrid;
+//
 @SuppressWarnings("serial")
 public class CronServlet extends HttpServlet {
 	private static final Logger _logger = Logger.getLogger(CronServlet.class.getName());
@@ -49,7 +50,7 @@ public class CronServlet extends HttpServlet {
 
 			try {
 				Message msg = new MimeMessage(session);
-				msg.setFrom(new InternetAddress("Tarang.Khandpur@utexas.edu"));
+				msg.setFrom(new InternetAddress("Tarang.Khandpur@ee461l-hw1-tk-karime.appspotmail.com"));
 				msg.setSubject("461L_HW1_Cron: Here's what has happened on the Blog Yesterday Since 5:00pm");
 				String str = new String();
 				Date yesterday = new Date();
@@ -96,3 +97,69 @@ public class CronServlet extends HttpServlet {
 		doGet(req, resp);
 	}
 }
+
+
+
+
+
+
+//-------------------------------------------SendGrid Below ------------------
+
+
+
+
+//
+//@SuppressWarnings("serial")
+//@WebServlet(name = "sendemail", value = "/send/email")
+//public class CronServlet extends HttpServlet {
+//
+//  @Override
+//  public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException,
+//      ServletException {
+//    final String sendgridApiKey = System.getenv("SENDGRID_API_KEY");
+//    final String sendgridSender = System.getenv("SENDGRID_SENDER");
+//    final String toEmail = req.getParameter("to");
+//    if (toEmail == null) {
+//      resp.getWriter()
+//          .print("Please provide an email address in the \"to\" query string parameter.");
+//      return;
+//    }
+//
+//    SendGrid sendgrid = new SendGrid(sendgridApiKey);
+//    SendGrid.Email email = new SendGrid.Email();
+//    email.addTo(toEmail);
+//    email.setFrom(sendgridSender);
+//    email.setSubject("This is a test email");
+//    email.setText("Example text body.");
+//
+//    try {
+//      SendGrid.Response response = sendgrid.send(email);
+//      if (response.getCode() != 200) {
+//        resp.getWriter().print(String.format("An error occured: %s", response.getMessage()));
+//        return;
+//      }
+//      resp.getWriter().print("Email sent.");
+//    } catch (SendGridException e) {
+//      throw new ServletException("SendGrid error", e);
+//    }
+//  }
+//}
+
+
+
+
+
+//-------------------------Sendgrid test 2 ------------
+
+//import sendGridGoogle.Sendgrid;
+////set credentials
+//Sendgrid mail = new Sendgrid("","");
+////set email data
+//mail.setTo(“foo@bar.com”)
+//.setFrom(“me@bar.com”)
+//.setSubject(“Subject goes here”)
+//.setText(“Hello World!”)
+//.setHtml(“Hello World!”);
+////send your message
+//mail.send();
+//
